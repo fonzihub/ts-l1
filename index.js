efd4fcb8b2cccd9d9b4b50f10a33fae041f5d58c
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-08-04 13:02:51 +0200

    tryng some ts

--- index.js
+++ index.js
@@ -1,2 +1,48 @@
 console.log("ciao sono typescript");
 var x = "mario";
+var n = 50;
+var firstName = 'Stefano';
+var addition = function (p1, p2) {
+    return p1 + p2;
+    // any
+};
+console.log(addition);
+// meglio tipizzare 
+var addition2 = function (p1, p2) {
+    return p1 + p2;
+};
+console.log(addition2(50, 50));
+var sayHello = function (name, prefix) {
+    // parametri opzionali vanno come ultimi
+    return (prefix || "Ciao") + ' ' + name;
+};
+console.log(sayHello('Giangiorgio', 'Buonasera'));
+console.log(sayHello('Stefano'));
+// si puo fare amche cosi
+var sayHello2 = function (name, prefix) {
+    if (prefix === void 0) { prefix = 'Ciao'; }
+    // parametri opzionali vanno come ultimi
+    return prefix + ' ' + name;
+};
+console.log(sayHello2('Giangiorgio', 'Buonasera'));
+console.log(sayHello2('Stefano'));
+var a = '50';
+// oggetti
+var specialDog = {
+    name: 'Pluto',
+    brand: 'Disney',
+    furColor: 'Blonde',
+    age: 5,
+    canFly: true,
+    species: {
+        name: 'Dog',
+        breed: 'Golden Retriever',
+    },
+    abilities: ['Bark', 'Eat', 'Play', 'Sleep'],
+    bark: function () {
+        return 'BAU';
+    }
+};
+var boh = specialDog.abilities.map(function (ab) {
+    return ab.length;
+});
--- index.ts
+++ index.ts
@@ -1,2 +1,61 @@
 console.log("ciao sono typescript")
 const x ="mario"
+
+const n: number = 50
+
+const firstName : string = 'Stefano'
+
+const addition = (p1,p2) =>{ 
+    return p1 + p2
+    // any
+}
+console.log(addition)
+
+// meglio tipizzare 
+const addition2 = (p1 :number ,p2:number ) =>{ 
+    return p1 + p2
+    
+}
+console.log(addition2(50,50))
+
+const sayHello = (name: string, prefix?: string) =>{
+    // parametri opzionali vanno come ultimi
+    return (prefix || "Ciao") + ' ' + name
+}
+console.log(sayHello('Giangiorgio', 'Buonasera'))
+console.log(sayHello('Stefano'))
+
+// si puo fare amche cosi
+const sayHello2 = (name: string, prefix: string = 'Ciao') => {
+    // parametri opzionali vanno come ultimi
+    return prefix  + ' ' + name
+}
+console.log(sayHello2('Giangiorgio', 'Buonasera'))
+console.log(sayHello2('Stefano'))
+
+// Type Alias 
+
+type MyType = string | number
+let a : MyType = '50'
+
+// oggetti
+
+const specialDog = {
+    name:'Pluto',
+    brand:'Disney',
+    furColor:'Blonde',
+    age:5,
+    canFly:true,
+    species:{
+        name:'Dog',
+        breed:'Golden Retriever',
+    },
+    abilities : ['Bark', 'Eat', 'Play', 'Sleep'],
+    bark:() => {
+        return 'BAU'
+    }
+}
+const boh = specialDog.abilities.map((ab) => {
+    return ab.length
+})
+    
